1660e6ac66fd8ae2109ea175236ea9f9fe38cdc7
stangah <pstanger@gmail.com>
2014-03-20 12:12:49 -0700

    Added gruntfile and build tasks

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['public/client/**/*.js'],
+        dest: 'public/dist/<%= pkg.name %>.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'public/dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+
+    jshint: {
+      files: [
+        'Gruntfile.js',
+        'app/**/*.js',
+        'public/**/*.js',
+        'lib/**/*.js',
+        './*.js',
+        'spec/**/*.js'
+      ],
+      options: {
+        // options here to override JSHint defaults
+        force: true,
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+
+    cssmin: {
+      prod: {
+        options: {
+          keepSpecialComments: 0
+        },
+        files: {
+          'public/style.min.css': 'public/style.css'
+        }
+      }
+    },
+
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint']
+    },
+
+    shell: {
+      localServer: {
+        command: 'nodemon',
+        options: {
+          stdout: true
+        }
+      },
+      prodServer: {
+        command: 'git push azure master',
+        options: {
+          stdout: true
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-shell');
+
+  grunt.registerTask('test', [
+    'jshint'
+  ]);
+
+  grunt.registerTask('build-dev', [
+    'jshint',
+    'concat',
+    'uglify',
+    'shell:localServer'
+  ]);
+
+  grunt.registerTask('build-prod', [
+    'jshint',
+    'concat',
+    'cssmin:prod',
+    'uglify',
+    'shell:prodServer'
+  ]);
+
+};
